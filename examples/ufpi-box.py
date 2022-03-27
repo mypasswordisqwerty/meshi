71fdda239a5920eb29d8ef5f7400003c4ea7911b
Br. John Fn None <bjohnfn@gmail.com>
2022-03-27 09:43:55 +0300

    UFPI box

---
+++ examples/ufpi-box.py
@@ -0,0 +1,26 @@
+from meshi import *
+
+init()
+
+w = 65
+l = 80
+
+box = product.box.SimpleBox(w, 30, l, pins={'height': 30}, bottom_thick=2)
+
+# front
+dz = -3
+Cube(40, 10, 6, at={'y': -l/2, 'z': dz}).substractFrom(box.front.panel)
+
+cs = -20+3
+z = 7 + dz
+Cylinder(4, 10, at=[cs, -l/2, z], rot={'x': 90}).substractFrom(box.front.panel)
+Cylinder(4, 10, at=[cs+10, -l/2, z], rot={'x': 90}).substractFrom(box.front.panel)
+Cylinder(4, 10, at=[cs+20, -l/2, z], rot={'x': 90}).substractFrom(box.front.panel)
+
+# back
+Cube(12, 10, 13, at={'y': l/2}).substractFrom(box.back.panel)
+x=6+8
+Cylinder(4, 10, at=[x, l/2, -2], rot={'x': 90}).substractFrom(box.back.panel)
+Cylinder(4, 10, at=[x, l/2, 3], rot={'x': 90}).substractFrom(box.back.panel)
+
+box.export("~/Downloads/")
